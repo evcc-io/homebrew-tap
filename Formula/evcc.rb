b2d8ecf4054b55d63e5f7baa7a55245eeca7a13a
andig <cpuidle@gmx.de>
2023-01-11 22:22:28 +0100

    Brew formula update for evcc version 0.111.1

--- Formula/evcc.rb
+++ Formula/evcc.rb
@@ -5,12 +5,12 @@
 class Evcc < Formula
   desc "Sonne tanken ☀️🚘"
   homepage "https://evcc.io"
-  version "0.111.0"
+  version "0.111.1"
   license "MIT"
 
   on_macos do
-    url "https://github.com/evcc-io/evcc/releases/download/0.111.0/evcc_0.111.0_macOS_all.tar.gz"
-    sha256 "08489fc6160630d76ee34d47d85dce4d03930dd9ba6de00dcc778969e816b0d5"
+    url "https://github.com/evcc-io/evcc/releases/download/0.111.1/evcc_0.111.1_macOS_all.tar.gz"
+    sha256 "fb8b3b0de48ae58af0d6b0c7ab379054721c2ce84336b8f1ac3d3dfb3a13a082"
 
     def install
       bin.install "evcc"
@@ -18,25 +18,25 @@ class Evcc < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/evcc-io/evcc/releases/download/0.111.0/evcc_0.111.0_linux_amd64.tar.gz"
-      sha256 "279ef36f27bdfa43a6c10697c7d237b69ac801b94b4b544a87858b4c6fe5fd1e"
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/evcc-io/evcc/releases/download/0.111.1/evcc_0.111.1_linux_armv6.tar.gz"
+      sha256 "6531892ee525006b10e2a2de30f0c10f228bce3d9df9c5a6016bfd4d2c1dd489"
 
       def install
         bin.install "evcc"
       end
     end
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/evcc-io/evcc/releases/download/0.111.0/evcc_0.111.0_linux_armv6.tar.gz"
-      sha256 "4a5c8e19f7a594bcad2d6ead042feef374d72ff0deff790321419c0a677c46fe"
+    if Hardware::CPU.intel?
+      url "https://github.com/evcc-io/evcc/releases/download/0.111.1/evcc_0.111.1_linux_amd64.tar.gz"
+      sha256 "40116b1bf95b3db135c7821ed20cd6ab2b3610d3f0fb2eadfc5bd138966ab072"
 
       def install
         bin.install "evcc"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/evcc-io/evcc/releases/download/0.111.0/evcc_0.111.0_linux_arm64.tar.gz"
-      sha256 "a4870f9b56cb823e5a73def17c995787d0a036887cc80fc96ae1c700840a55a8"
+      url "https://github.com/evcc-io/evcc/releases/download/0.111.1/evcc_0.111.1_linux_arm64.tar.gz"
+      sha256 "f7e6b0dbafe74783ff81d6039f1df93dfb395da8220e99ada47560facebb297e"
 
       def install
         bin.install "evcc"
